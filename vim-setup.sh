90f02a35e1ee5845a796321f02cdd5b7b85d5a54
RTongue <r.tongue@gmail.com>
2024-01-18 22:48:52 -0500

    Added setup for the vim-surround plugin

--- vim-setup.sh
+++ vim-setup.sh
@@ -8,3 +8,9 @@ git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 # Install syntastic
 cd ~/.vim/bundle && \
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+
+# Install vim-surround
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
